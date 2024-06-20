3e3c1e65dd4fd591d344d507cbcc16b41c8e5771
franklin-raph <igboekwulusifranklin@gmail.com>
2024-06-20 15:51:23 +0100

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -20,7 +20,9 @@ import { IoChevronBack } from "react-icons/io5";
 import { MdOutlineRecommend } from "react-icons/md";
 import { MdOutlineStarRate } from "react-icons/md";
 import { PiClockClockwise } from "react-icons/pi";
-
+import Marquee from "react-fast-marquee";
+import { PiDotDuotone } from "react-icons/pi";
+import { MdSportsFootball } from "react-icons/md";
 
 
 
@@ -92,6 +94,8 @@ const Home = () => {
     prevArrow: <GamePrevArrow />,
   };
 
+  const announcement = ["I can be a React component, multiple React components, or just some text.", "I can be a React component, multiple React components, or just some text.", "I can be a React component, multiple React components, or just some text.", "I can be a React component, multiple React components, or just some text."]
+
 
 
 
@@ -114,6 +118,22 @@ const Home = () => {
           <img src="./images/880x4010.avif" className='rounded-[6px] h-full object-contain' alt="" />
         </div>
       </Slider>
+
+      <div className='bg-[#0D0D11] flex items-center py-1 mt-7 rounded-l-[10px]'>
+        <img src="./images/notification.svg" className='p-3' alt="" />
+        <Marquee>
+          {
+            announcement.map((item, index) => {
+              return(
+                <div key={index} className='flex items-center gap-1 text-color text-[14px] mx-[20px]'>
+                  <PiDotDuotone className='text-[20px]'/>
+                  <p className='text-white'>{item}</p>
+                </div>
+              )
+            })
+          }
+        </Marquee>
+      </div>
 
       <div className='mt-[3rem]'>
         <div className='flex items-center justify-between mb-4'>
@@ -257,6 +277,55 @@ const Home = () => {
             </div>
           </Slider>
         </div>
+
+        <div className='mt-[3rem]'>
+        <div className='flex items-center justify-between mb-4'>
+          <div className='flex items-center gap-2 text-color text-[22px]'>
+            <MdSportsFootball className='text-[30px]'/>
+            <p className='text-white font-[700]'>Popular</p>
+          </div>
+          <p className='mr-[6rem] text-color cursor-pointer bg-[#1f29374d] py-[6px] px-[10px] rounded-[5px] font-[600]'>More 22</p>
+        </div>
+
+        <div >
+          <Slider {...gamesCarouselSettings}>
+            <div className='rounded-[6px]'>
+              <img src="./images/games.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/games4.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/games2.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/games5.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/games1.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/games6.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/game0.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/game02.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/game03.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/game01.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+            <div className='rounded-[6px]'>
+              <img src="./images/game7.avif" className='rounded-[6px] h-full object-contain' alt="" />
+            </div>
+          </Slider>
+        </div>
+      </div>
+
       </div>
     </div>
   )
