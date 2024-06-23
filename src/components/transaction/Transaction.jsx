8d5abfaa405b8f0f51e78af136b93d120d11cf70
franklin-raph <igboekwulusifranklin@gmail.com>
2024-06-23 15:36:19 +0100

    update

--- src/components/transaction/Transaction.jsx
+++ src/components/transaction/Transaction.jsx
@@ -4,6 +4,8 @@ import { IoChevronDown } from "react-icons/io5";
 
 const Transaction = ({setCurrentModal}) => {
 
+    const [dropDown, setDropDown] = useState(false)
+
     const [dateDropDown, setDateDropDown] = useState(false)
     const dateArray = ["Today", "Yesterday", "Last 3 days", "Last 7 days", "Last 15 days", "Last 30 days"]
     const [selectedDate, setSelectedDate] = useState(dateArray[0])
@@ -26,17 +28,17 @@ const Transaction = ({setCurrentModal}) => {
             </div>
             <div className='p-6'>
                 <div className='flex items-center justify-between gap-2'>
-                    <div className='cursor-pointer relative flex items-center justify-between w-full bg-[#2A2F39] p-2 rounded-[6px]' onClick={() => setDateDropDown(!dateDropDown)}>
+                    <div className='cursor-pointer relative flex items-center justify-between w-full bg-[#2A2F39] p-2 rounded-[6px]' onClick={() => setDropDown( dropDown === 'date' ? false : 'date' )}>
                         <p className='text-[14px]'>{selectedDate}</p>
                         <IoChevronDown />
                         {
-                            dateDropDown&&
+                            dropDown === 'date' &&
                             <div className='absolute bg-[#2A2F39] top-[45px] rounded-[6px] w-full left-0'>
                                 {
                                     dateArray.map((item, index) => {
                                         return (
                                             <p key={index} onClick={() => {
-                                                setDateDropDown(!dateDropDown)
+                                                setDropDown(false)
                                                 setSelectedDate(item)
                                             }} className='text-[14px] cursor-pointer px-4 py-2 hover:bg-[#23262F]'>{item}</p>
                                         )
@@ -45,17 +47,17 @@ const Transaction = ({setCurrentModal}) => {
                             </div>
                         }
                     </div>
-                    <div className='cursor-pointer relative flex items-center justify-between w-full bg-[#2A2F39] p-2 rounded-[6px]' onClick={() => setWithdrawDropDown(!withdrawDropDown)}>
+                    <div className='cursor-pointer relative flex items-center justify-between w-full bg-[#2A2F39] p-2 rounded-[6px]' onClick={() => setDropDown( dropDown === 'withdraw' ? false : 'withdraw' )}>
                         <p className='text-[14px]'>{selectedWithdrawal}</p>
                         <IoChevronDown />
                         {
-                            withdrawDropDown&&
+                            dropDown === 'withdraw' &&
                             <div className='absolute bg-[#2A2F39] top-[45px] rounded-[6px] w-full left-0'>
                                 {
                                     withdrawArray.map((item, index) => {
                                         return (
                                             <p key={index} onClick={() => {
-                                                setWithdrawDropDown(!withdrawDropDown)
+                                                setDropDown(false)
                                                 setSelectedSelectedWithdrawal(item)
                                             }} className='text-[14px] cursor-pointer px-4 py-2 hover:bg-[#23262F]'>{item}</p>
                                         )
@@ -64,17 +66,17 @@ const Transaction = ({setCurrentModal}) => {
                             </div>
                         }
                     </div>
-                    <div className='cursor-pointer relative flex items-center justify-between w-full bg-[#2A2F39] p-2 rounded-[6px]' onClick={() => setStatusDropDown(!statusDropDown)}>
+                    <div className='cursor-pointer relative flex items-center justify-between w-full bg-[#2A2F39] p-2 rounded-[6px]' onClick={() => setDropDown( dropDown === 'status' ? false : 'status' )}>
                         <p className='text-[14px]'>{selectedStatus}</p>
                         <IoChevronDown />
                         {
-                            statusDropDown&&
+                            dropDown === 'status' &&
                             <div className='absolute bg-[#2A2F39] top-[45px] rounded-[6px] w-full left-0'>
                                 {
                                     statusArray.map((item, index) => {
                                         return (
                                             <p key={index} onClick={() => {
-                                                setStatusDropDown(!statusDropDown)
+                                                setDropDown(false)
                                                 setSelectedSelectedStatus(item)
                                             }} className='text-[14px] cursor-pointer px-4 py-2 hover:bg-[#23262F]'>{item}</p>
                                         )
@@ -84,7 +86,7 @@ const Transaction = ({setCurrentModal}) => {
                         }
                     </div>
                 </div>
-                <div className='my-3 h-[400px] bg-[#191B21] rounded-[6px] p-4 flex items-center'>
+                <div className='my-3 h-[400px] bg-[#191B21] rounded-[6px] p-4 flex items-center' onClick={() => setDropDown(false)}>
                     <div className='flex items-center justify-center flex-col'>
                         <img src="./images/empty.avif" className='w-[40%]' alt="" />
                         <p className='text-color'>Oops! No data yet!</p>
