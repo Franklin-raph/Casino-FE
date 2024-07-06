cdb77f92c15c5ee2280bfa13c89a1b424afc150d
franklin-raph <igboekwulusifranklin@gmail.com>
2024-07-06 13:37:45 +0100

    update

--- src/components/bottom-nav/BottomNav.jsx
+++ src/components/bottom-nav/BottomNav.jsx
@@ -10,6 +10,16 @@ import { MdAutoGraph } from "react-icons/md";
 const BottomNav = ({setProfileNav, setCurrentModal,}) => {
 
     const navigate = useNavigate()
+    const user = JSON.parse(localStorage.getItem('user_details'))
+
+    function checkUserAANdNavigate(path){
+        setProfileNav(false)
+        if(!user){
+            setCurrentModal('login')
+        }else{
+            setCurrentModal(path)
+        }
+    }
 
   return (
     <div className='flex items-center top-bg justify-between px-[1.5rem] py-[0.5rem] fixed bottom-0 w-full z-[10001]'>
@@ -21,20 +31,20 @@ const BottomNav = ({setProfileNav, setCurrentModal,}) => {
             <HiOutlineHome className='text-[22px] text-color'/>
             <p className='text-white text-[12px] font-[600]'>Home</p>
         </div>
-        <div className='flex items-center flex-col text-center cursor-pointer gap-[2px]'>
+        <div 
+            onClick={() => setCurrentModal('coming-soon')}
+            className='flex items-center flex-col text-center cursor-pointer gap-[2px]'>
             <MdAutoGraph className='text-[22px] text-color'/>
             <p className='text-white text-[12px] font-[600]'>Promotion</p>
         </div>
         <div onClick={() => {
-            setProfileNav(false)
-            setCurrentModal('deposit')
+            checkUserAANdNavigate('deposit')
           }} className='flex items-center flex-col text-center cursor-pointer gap-[2px]'>
             <FaPiggyBank className='text-[22px] text-color'/>
             <p className='text-white text-[12px] font-[600]'>Deposit</p>
         </div>
         <div onClick={() => {
-            setProfileNav(false)
-            setCurrentModal('profile')
+            checkUserAANdNavigate('profile')
           }} className='flex items-center flex-col text-center cursor-pointer gap-[2px]'>
             <FaRegCircleUser className='text-[22px] text-color'/>
             <p className='text-white text-[12px] font-[600]'>Profile</p>
